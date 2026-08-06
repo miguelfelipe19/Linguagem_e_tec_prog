4b4f223acd3955e565dfcc25df259d3507473195
Miguel Felipe <miguelfelipemardegan2@gmail.com>
2026-08-06 11:31:58 -0300

    Add files via upload

---
+++ mainaula1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float a,b,c;
+	a = 10;
+	b = 75;
+	c = a+b; 
+	printf("A soma de %f / %f = %f", a,b,c );
+	
+	return 0;	
+}
